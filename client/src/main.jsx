c17495225dba581e28f41d4a2823346ac0bfb7f4
salmanfxrsi <salmanxprivate@gmail.com>
2025-02-25 00:43:32 +0600

    feat: (client): set up latest version of React in the project

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import Router from "./routes/router.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById("root");
+
+createRoot(rootElement).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  </StrictMode>
+);
---
+++ client/src/routes/Router.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router";
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route index element={<h1>Hello</h1>} />
+    </Routes>
+  );
+};
+
+export default Router;

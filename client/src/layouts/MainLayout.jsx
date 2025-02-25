1290f6805134cad6fd44a7ccc9286038863d71f6
salmanfxrsi <salmanxprivate@gmail.com>
2025-02-26 00:26:07 +0600

    feat (client): add stunning background photo in MainLayout
    - Added an eye-catching photo as the background for the MainLayout.
    - This background serves as the default page of the application, enhancing the visual appeal and user experience.

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -3,7 +3,12 @@ import Navbar from "../components/shared/Navbar";
 
 const MainLayout = () => {
   return (
-    <div className="bg-[#010313] min-h-screen py-2">
+    <div
+      className="bg-[#010313] min-h-screen py-2 bg-bottom bg-no-repeat"
+      style={{
+        backgroundImage: "url(https://i.ibb.co.com/d4FDbvyw/city.webp)",
+      }}
+    >
       {/* Navigation Bar */}
       <nav className="w-11/12 mx-auto">
         <Navbar />

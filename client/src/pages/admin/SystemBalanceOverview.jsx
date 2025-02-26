fd662b76e54765c863d8b2578cefd56782096127
salmanfxrsi <salmanxprivate@gmail.com>
2025-02-26 11:16:25 +0600

    feat (client): complete System Balance Overview page with total money and income display

--- client/src/pages/admin/SystemBalanceOverview.jsx
+++ client/src/pages/admin/SystemBalanceOverview.jsx
@@ -1,14 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
-import useAxiosSecure from "../../hooks/useAxiosSecure";
 import { useState } from "react";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useUserInfo from "../../hooks/useUserInfo";
 import taka from "../../assets/taka.png";
 
 const SystemBalanceOverview = () => {
   const axiosSecure = useAxiosSecure();
   const [showBalance, setShowBalance] = useState(false);
+  const { isLoading: userLoading, userInfo } = useUserInfo();
 
   // Fetching System Balance
-  const { isLoading, data: systemBalance } = useQuery({
+  const { isLoading: balanceLoading, data: systemBalance } = useQuery({
     queryKey: ["systemBalance"],
     queryFn: async () => {
       const { data } = await axiosSecure.get("/system-balance");
@@ -16,11 +18,11 @@ const SystemBalanceOverview = () => {
     },
   });
 
-  if (isLoading) return null;
+  if (userLoading || balanceLoading) return null;
 
   return (
     <div className="flex justify-center">
-      <div className="stats bg-gradient-to-bl from-violet-500 to-fuchsia-500 text-white p-4 rounded-lg shadow-lg">
+      <div className="stats bg-gradient-to-bl from-violet-500 to-fuchsia-500 text-white p-5 rounded-lg shadow-lg">
         {/* Balance Check Credential */}
         <div className="stat">
           <p className="text-sm font-medium">Balance Check Credential</p>
@@ -29,9 +31,9 @@ const SystemBalanceOverview = () => {
           </p>
           <button
             onClick={() => setShowBalance(!showBalance)}
-            className="bg-white text-black rounded-sm mt-2 px-3 py-1 text-sm font-medium transition duration-200 hover:bg-gray-200"
+            className="bg-white text-black rounded-md mt-2 px-3 py-1 text-sm font-medium transition duration-200 hover:bg-gray-200 cursor-pointer"
           >
-            {showBalance ? "Hide Balance" : "Show Balance"}
+            {showBalance ? "Click To Hide" : "Click To Show"}
           </button>
         </div>
 
@@ -42,6 +44,17 @@ const SystemBalanceOverview = () => {
             <img className="w-8 h-8" src={taka} alt="Taka Icon" />
             <h1 className="text-2xl font-bold">
               {showBalance ? systemBalance?.total_money : "******"}
+            </h1>
+          </div>
+        </div>
+
+        {/* Total Income */}
+        <div className="stat">
+          <h2 className="text-lg font-semibold">Total Income</h2>
+          <div className="mt-2 flex items-center gap-2">
+            <img className="w-8 h-8" src={taka} alt="Taka Icon" />
+            <h1 className="text-2xl font-bold">
+              {showBalance ? userInfo?.balance : "******"}
             </h1>
           </div>
         </div>
